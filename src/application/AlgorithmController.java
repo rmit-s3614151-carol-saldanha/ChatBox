b5be70155b3bfd1abea44360e8d2a28823b02055
rmit-s3614151-carol-saldanha <s3614151@student.rmit.esu.au>
2017-04-29 11:42:51 +1000

    Disable buttons

--- src/application/AlgorithmController.java
+++ src/application/AlgorithmController.java
@@ -1,7 +1,5 @@
 package application;
 
-import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -16,10 +14,9 @@ public class AlgorithmController {
 	@FXML
 	private Button RSA;
 
-	
 	@FXML
 	private Button Paillier;
-	
+
 	@FXML
 	private Label label;
 
@@ -31,51 +28,58 @@ public class AlgorithmController {
 
 	@FXML
 	private TextArea msgBox2;
-	
+
 	@FXML
-    private TextField name;
-	
+	private TextField name;
+
 	@FXML
-    private TextField ipAddress;
-	
+	private TextField ipAddress;
+
 	@FXML
-    private Button start;
-	
+	private Button start;
+
 	@FXML
 	private Button readNewMsg;
-	
+
 	private FileHandle rsaFile = null;
-	
-	
+
+	public void validate() {
+
+		
+		RSA.setDisable(true);
+		Paillier.setDisable(true);
+		elgamal.setDisable(true);
+		readNewMsg.setDisable(true);
+		label.setText("Enter your name and the Ip Address of the person you want to chat with");
+
+	}
+
 	public void onClickStart(ActionEvent event) {
-		if(name.getText().isEmpty() || ipAddress.getText().isEmpty())
-		{
-			//start.setDisable(true);
-			RSA.setDisable(true);
-			Paillier.setDisable(true);
-			elgamal.setDisable(true);
-			readNewMsg.setDisable(true);
-			label.setText("Enter your name and the Ip Address of the person you want to chat with");
-		}
-		else
-		{
-			//start.setDisable(false);
+		if (name.getText().isEmpty() || ipAddress.getText().isEmpty()) {
+			validate();
+
+		} else {
+			
 			RSA.setDisable(false);
 			Paillier.setDisable(false);
 			elgamal.setDisable(false);
 			readNewMsg.setDisable(false);
-		this.rsaFile= new FileHandle(ipAddress.getText());
+			this.rsaFile = new FileHandle(ipAddress.getText());
 		}
 	}
 
-
 	public void onClickRSA(ActionEvent event) {
-		rsa.setMessage(name.getText()+": "+msgBox1.getText());
-		rsa.setEncryptedMessage("RSA|");
-		rsa.computeEncryptedMessage();
-		System.out.println("New message: "+rsa.getEncryptedMessage());
+		if (name.getText().isEmpty() || ipAddress.getText().isEmpty()) {
+			validate();
+
+		} else {
+			rsa.setMessage(name.getText() + ": " + msgBox1.getText());
+			rsa.setEncryptedMessage("RSA|");
+			rsa.computeEncryptedMessage();
+			System.out.println("New message: " + rsa.getEncryptedMessage());
 			rsaFile.writeToFile(rsa.getEncryptedMessage());
-		
+		}
+
 	}
 
 	public void onClickPaillier(ActionEvent event) {
@@ -85,18 +89,20 @@ public class AlgorithmController {
 	public void onClickElegmal(ActionEvent event) {
 		System.out.println("Elegmal");
 	}
-	
-	
+
 	public void onClickRead(ActionEvent event) {
-		String encryptedMsg = rsaFile.readFile();
-		rsa.setDepcryptedMessage("");
-		rsa.setEncryptedMessage(encryptedMsg);
-		rsa.decryptEncryptedMessage();
-		System.out.println(rsa.getDepcryptedMessage());
-		msgBox2.appendText("\n" + rsa.getDepcryptedMessage());
-		
+		if (name.getText().isEmpty() || ipAddress.getText().isEmpty()) {
+			validate();
+
+		} else {
+
+			String encryptedMsg = rsaFile.readFile();
+			rsa.setDepcryptedMessage("");
+			rsa.setEncryptedMessage(encryptedMsg);
+			rsa.decryptEncryptedMessage();
+			System.out.println(rsa.getDepcryptedMessage());
+			msgBox2.appendText("\n" + rsa.getDepcryptedMessage());
+		}
 	}
-
-
 
 }
